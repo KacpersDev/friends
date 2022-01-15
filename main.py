5119a4cbce304a69659d99477b109449c9450e89
kacper <vkacperpl@gmail.com>
2022-01-15 19:44:10 +0100

    Project has been completed

--- main.py
+++ main.py
@@ -17,28 +17,40 @@ def party():
                 guests_value = input()
                 friends[guests_value] = 0
                 guests_name.append(guests_value)
-def lucky():
-    print('Do you want to use the "Who is lucky?" feature? Write Yes/No:')
-    answer = str(input())
-    if answer == "No":
-        print()
-        return print("No one is going to be lucky")
-    elif answer == "Yes":
-        luck_number = guests - 1
-        luck = random.randint(0, luck_number)
-        print()
-        print(f"{guests_name[luck]} is the lucky one!")
 
 party()
 if guests > 0:
     print()
     total = int(input("Enter the total bill value:"))
-    per_user = round(total / guests, 2)
-    for f in friends:
-        if per_user % 2 == 0:
-            round(per_user, 2)
-            friends[f] = int(per_user)
-        else:
-            friends[f] = per_user
     print()
-    lucky()
+    print('Do you want to use the "Who is lucky?" feature? Write Yes/No:')
+    answer = str(input())
+    if answer == "No":
+        print()
+        per_user = round(total / guests, 2)
+        for f in friends:
+            if per_user % 2 == 0:
+                round(per_user, 2)
+                friends[f] = int(per_user)
+            else:
+                friends[f] = per_user
+        print("No one is going to be lucky")
+        print()
+        print(friends)
+    elif answer == "Yes":
+        luck_number = guests - 1
+        luck = random.randint(0, luck_number)
+        print()
+        print(f"{guests_name[luck]} is the lucky one!")
+        guests = guests - 1
+        per_user = round(total / guests, 2)
+        for f in friends:
+            if per_user % 2 == 0:
+                round(per_user, 2)
+                friends[f] = int(per_user)
+                friends.update({f"{guests_name[luck]}": 0})
+            else:
+                friends[f] = per_user
+                friends.update({f"{guests_name[luck]}": 0})
+        print()
+        print(friends)
