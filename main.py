a3bf5e708b2786fd644c4b5dc164cd15f4d55eb4
kacper <vkacperpl@gmail.com>
2022-01-15 16:29:45 +0100

    Stage 3 Completed Added Who is lucky feature

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import random
+
 print("Enter the number of friends joining (including you): ")
 guests = int(input())
 friends = dict()
+guests_name = []
 
 def party():
     if guests <= 0:
+        print()
         return print("No one is joining for the party")
     else:
         if guests > 0:
@@ -12,6 +16,19 @@ def party():
             for guests_value in range(guests):
                 guests_value = input()
                 friends[guests_value] = 0
+                guests_name.append(guests_value)
+def lucky():
+    print('Do you want to use the "Who is lucky?" feature? Write Yes/No:')
+    answer = str(input())
+    if answer == "No":
+        print()
+        return print("No one is going to be lucky")
+    elif answer == "Yes":
+        luck_number = guests - 1
+        luck = random.randint(0, luck_number)
+        print()
+        print(f"{guests_name[luck]} is the lucky one!")
+
 party()
 if guests > 0:
     print()
@@ -24,4 +41,4 @@ if guests > 0:
         else:
             friends[f] = per_user
     print()
-    print(friends)
+    lucky()
